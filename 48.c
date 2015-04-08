53b4320a0956c6b0de540276a9006ad53dfd1ed6
John Spence <jespence96@gmail.com>
2015-04-08 14:03:22 -0700

    First commit

---
+++ 48.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int count(char *ptr);
+
+int main(void)
+{
+	char userin[100];
+	printf("Enter a string: ");
+	gets(userin);
+
+	printf("%s\n", userin);
+	printf("%s\n", userin);
+}
+	
+	
