920f8d0409b995e626df70e22de12acb953a1d57
John Spence <jespence96@gmail.com>
2015-04-08 14:31:26 -0700

    Second commit

--- 48.c
+++ 48.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include <string.h>
 int count(char *ptr);
+int charcount(char *ptr, int size);
 
 int main(void)
 {
 	char userin[100];
+	int n;
 	printf("Enter a string: ");
 	gets(userin);
 
@@ -12,6 +14,16 @@ int main(void)
 	printf("%s\n", userin);
 
 	printf("Number of words: %d\n", count(userin));
+	printf("How many characters to display: ");
+	
+	scanf("%d", &n);
+	while (n > strlen(userin))
+	{
+		printf("String to short for this number! Enter new number: ");
+		scanf("%d", &n);
+	}
+	charcount(userin, n);
+
 }
 	
 int count(char *ptr)
@@ -27,4 +39,9 @@ int count(char *ptr)
 	}
 	return c;
 }
-	
+
+int charcount(char *ptr, int size)
+{
+	for (int i = 0; i < size; i++)
+		putchar(ptr[i]);
+}	
